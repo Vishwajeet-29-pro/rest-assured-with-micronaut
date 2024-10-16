bab1c44b191a1f25ec392641e5bdeb4d3b032123
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-10-16 22:00:30 +0530

    ci: Add REST-assured dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     compileOnly("io.micronaut:micronaut-http-client")
     runtimeOnly("ch.qos.logback:logback-classic")
     testImplementation("io.micronaut:micronaut-http-client")
+    testImplementation("io.micronaut.test:micronaut-test-rest-assured")
 }
 
 
