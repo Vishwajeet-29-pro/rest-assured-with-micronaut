2ec1a09df01f1c328940303d61ba630c509acf10
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-10-17 20:55:23 +0530

    feat: Add endpoint which returns json data and make test pass.

--- src/main/java/com/rest/assure/HelloController.java
+++ src/main/java/com/rest/assure/HelloController.java
@@ -5,6 +5,9 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 
+import java.util.Collections;
+import java.util.Map;
+
 @Controller("/hello")
 public class HelloController {
 
@@ -12,5 +15,12 @@ public class HelloController {
     @Produces(MediaType.TEXT_PLAIN)
     public String index() {
         return "Hello World";
+    }
+
+    @Get("/my-endpoint")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> myEndpoint() {
+        String message = "Hello Micronaut";
+        return Collections.singletonMap("message", message);
     }
 }
