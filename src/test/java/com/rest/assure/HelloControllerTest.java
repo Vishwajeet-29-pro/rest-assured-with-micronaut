54b294d6a620545283a5649b8008ce0b7b602112
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-10-25 23:04:27 +0530

    test: Add failing test for post request

--- src/test/java/com/rest/assure/HelloControllerTest.java
+++ src/test/java/com/rest/assure/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.rest.assure;
 
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 
 @MicronautTest
@@ -46,6 +48,24 @@ public class HelloControllerTest {
                 .statusCode(200)
                 .contentType(ContentType.JSON)
                 .body("id", equalTo(1))
+                .body("name", equalTo("John Doe"));
+    }
+
+    @Test
+    public void testPostRequest(RequestSpecification spec) {
+        String request = "{\"userId\": \"1\", \"name\": \"John Doe\"}";
+
+        UserRequest userRequest = new UserRequest(1, "John Doe");
+
+        spec
+                .contentType(ContentType.JSON)
+                .body(userRequest)
+                .when()
+                .post("/hello/my-post-request")
+                .then()
+                .statusCode(201)
+                .contentType(ContentType.JSON)
+                .body("userId", notNullValue())
                 .body("name", equalTo("John Doe"));
     }
 }
